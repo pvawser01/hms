338e639228536ce8a9f0eaaf2a0b974c5d8fedde
Peter and Komang Vawser <peterandkomang@Macintosh-731.local>
2015-07-13 22:19:53 +0930

    Added inital test for PatientCategory

--- app/models/patient_category.rb
+++ app/models/patient_category.rb
@@ -2,4 +2,6 @@ class PatientCategory < ActiveRecord::Base
   belongs_to :hospital
   has_many :wards
   has_many :patients
+  
+  validates_presence_of :name
 end
--- test/models/patient_category_test.rb
+++ test/models/patient_category_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class PatientCategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test 'PatientCategory saves with valid name' do
+    patient_category = hospitals(:one).patient_categories.create(name: 'TEST CAT')
+    assert patient_category.valid?, 'PatientCategory did not save when name provided.'   
+  end  
+  
+  test 'PatientCategory wont save without name' do
+    patient_category = hospitals(:one).patient_categories.create({})
+    assert_not patient_category.valid?, 'The PatientCategory should not save when missing name.'   
+  end
+  
 end
