f964b3bf50d694a646fe59fe3b76827059b54d27
Peter and Komang Vawser <peterandkomang@Macintosh-731.local>
2015-07-13 22:14:56 +0930

    Added inital test for beds

--- app/models/bed.rb
+++ app/models/bed.rb
@@ -1,4 +1,14 @@
 class Bed < ActiveRecord::Base
   belongs_to :room
   has_one :patient
+  
+  validates_presence_of :bed_number
+  validate :can_accommodate_bed
+  
+private
+  def can_accommodate_bed
+    if room.beds.size >= 4
+      errors.add(:bed, "This bed can not be accommodated in the room.")
+    end
+  end
 end
--- test/models/bed_test.rb
+++ test/models/bed_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class BedTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test 'Bed saves with bed_number provided' do
+    bed = rooms(:inf1).beds.create(bed_number: 'EME1-B1')
+    assert bed.valid?, 'Bed did not save when bed_number provided.'   
+  end  
+    
+  test 'Bed wont save without bed_number' do
+    bed = rooms(:inf1).beds.create({})
+    assert_not bed.valid?, 'The bed should not save when missing bed_number.'
+  end
+  
+  test 'Bed can not be saved if associated room already has 4 beds' do
+    bed = rooms(:eme1).beds.create(bed_number: 'Spare')
+    assert_not bed.valid?, 'The bed should not save when room is full.'
+  end
+  
 end
