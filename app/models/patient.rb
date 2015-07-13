dbce17cca8a5f4abfff01c9c64e0f2a36d481ac4
Peter and Komang Vawser <peterandkomang@Macintosh-731.local>
2015-07-13 22:27:02 +0930

    Added inital tests for patient

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,5 +2,6 @@ class Patient < ActiveRecord::Base
   belongs_to :bed
   belongs_to :patient_category
   
+  validates_presence_of :first_name, :surname, :date_of_birth
   
 end
--- test/models/patient_test.rb
+++ test/models/patient_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class PatientTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test 'Patient saves with valid parameters' do
+    patient = Patient.create(first_name: 'Ben', surname: 'Harper', date_of_birth: '01 Jan 1963')
+    assert patient.valid?, 'Patient did not save when valid parameters provided.'   
+  end  
+  
+  test 'Patient wont save without first name' do
+    patient = Patient.create(surname: 'Harper', date_of_birth: '01 Jan 1963')
+    assert_not patient.valid?, 'The Patient should not save when missing first name.'   
+  end
+  
+  test 'Patient wont save without surname' do
+    patient = Patient.create(first_name: 'Ben', date_of_birth: '01 Jan 1963')
+    assert_not patient.valid?, 'The Patient should not save when missing surname.'   
+  end
+  
+  test 'Patient wont save without date_of_birth' do
+    patient = Patient.create(first_name: 'Ben', surname: 'Harper')
+    assert_not patient.valid?, 'The Patient should not save when missing date_of_birth.'   
+  end
+  
 end
