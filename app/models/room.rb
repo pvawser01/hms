2e9e8665269578526054fd183cd4cb1abf5935e0
Peter and Komang Vawser <peterandkomang@Macintosh-731.local>
2015-07-13 21:50:06 +0930

    Added inital test for rooms

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,6 @@
 class Room < ActiveRecord::Base
   belongs_to :ward
   has_many :beds
+  
+  validates_presence_of :name
 end
--- test/models/room_test.rb
+++ test/models/room_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class RoomTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'Room saves with valid paramters' do
+    ward = wards(:emergency).rooms.create(name: 'my room')
+    assert ward.valid?, 'Room did not save when name provided.'   
+  end  
+    
+  test 'Room wont save without name' do
+    ward = wards(:emergency).rooms.create({})
+    assert_not ward.valid?, 'The room should not save when missing name.'
+  end
+
 end
